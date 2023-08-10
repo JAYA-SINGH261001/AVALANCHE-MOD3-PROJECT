0dbe61e1332592220cb223aa452f616e2a2dbff6
JAYA-SINGH261001 <137693712+JAYA-SINGH261001@users.noreply.github.com>
2023-08-10 16:15:55 +0530

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,17 +1,16 @@
-const { ethers } = require("hardhat");
-
 async function main() {
   const MyToken = await ethers.getContractFactory("MyToken");
-  const myToken = await MyToken.deploy("My Token", "MTK");
-
+  const myToken = await MyToken.deploy();
+  
   await myToken.deployed();
-
+  
   console.log("MyToken deployed to:", myToken.address);
 }
 
 main()
   .then(() => process.exit(0))
-  .catch((error) => {
+  .catch(error => {
     console.error(error);
     process.exit(1);
   });
+
